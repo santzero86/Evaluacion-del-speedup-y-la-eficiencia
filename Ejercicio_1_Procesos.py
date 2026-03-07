6315556902b2989db47f36b7e42ab62c64b7e5da
Santiago Guerrero <santiago.david.guerrero@correounivalle.edu.co>
2026-03-07 18:41:42 -0500

    First Commit

---
+++ Ejercicio_1_Procesos.py
@@ -0,0 +1,54 @@
+import multiprocessing
+import time
+import numpy as np
+
+
+def suma_parcial(datos):
+    return sum(datos)
+
+
+def ejecutar_paralelo(datos, num_procesos):
+    inicio = time.time()
+
+    tamaño = len(datos) // num_procesos
+    partes = [datos[i*tamaño:(i+1)*tamaño] for i in range(num_procesos)]
+
+    if len(datos) % num_procesos != 0:
+        partes[-1] = np.concatenate((partes[-1], datos[num_procesos*tamaño:]))
+
+    with multiprocessing.Pool(processes=num_procesos) as pool:
+        resultados = pool.map(suma_parcial, partes)
+
+    resultado_final = sum(resultados)
+    fin = time.time()
+
+    return resultado_final, fin - inicio
+
+
+if __name__ == "__main__":
+
+    N = 100_000_000
+    datos = np.random.randint(0, 10, N)
+
+    #Secuencial 
+    inicio = time.time()
+    resultado_sec = sum(datos)
+    fin = time.time()
+
+    tiempo_sec = fin - inicio
+
+    print("Secuencial:")
+    print("Tiempo:", tiempo_sec, "segundos\n")
+
+    #Paralelo
+
+    for procesos in [2, 4, 8, 16, 32, 64]:
+        resultado_par, tiempo_par = ejecutar_paralelo(datos, procesos)
+
+        speedup = tiempo_sec / tiempo_par
+
+        print(f"Paralelo ({procesos} procesos):")
+        print("Tiempo:", tiempo_par, "segundos")
+        print("Speedup:", speedup)
+        print("Eficiencia: ", speedup/procesos)
+        print()
---
+++ Ejercicio_2_Hilos.py
@@ -0,0 +1,52 @@
+import os
+import numpy as np
+import time
+
+#Numero de hilos para la ejecución de operaciones vectorizadas
+
+base = 2
+
+N = 10_000_000
+
+A = np.random.rand(N)
+B = np.random.rand(N)
+
+#Bucle for para sumar los elementos de A y B y almacenar el resultado en C
+print("Ejecucion con for...")
+inicio = time.time()
+
+C = [0]*N
+
+for i in range(N):
+    C[i] = A[i] + B[i]
+
+fin = time.time()
+print("Tiempo con for:", fin - inicio)
+
+#Vectorización con NumPy para sumar los elementos de A y B y almacenar el resultado en C, 
+#utilizando diferentes números de hilos y midiendo el tiempo de ejecución para cada caso.
+
+print("\nEjecucion con NumPy utilizando diferentes números de hilos:")
+for i in range(4):
+    NUM_HILOS = base**i
+
+    print("\nEjecutando con ", NUM_HILOS, " hilos, iteracion...")
+    os.environ["OMP_NUM_THREADS"] = str(NUM_HILOS)
+    os.environ["OPENBLAS_NUM_THREADS"] = str(NUM_HILOS)
+    os.environ["MKL_NUM_THREADS"] = str(NUM_HILOS)
+    tiempos = []
+
+    for j in range(5):
+        inicio = time.time()
+        C = A + B
+        fin = time.time()
+        tiempos.append(fin - inicio)
+        print("Ejecucion", j+1, ":", fin - inicio)
+    
+    promedio = sum(tiempos) / len(tiempos)
+
+    print("Tiempo con NumPy ", base**i, " hilos: ", promedio)
+
+
+
+
